ebb646a4513ecedc46f0ce949ffbf13c42bfe437
Junior Mizhquiri <Junioromizhquiri@gmail.com>
2021-08-12 12:46:28 -0400

    solved return license or return empty string

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license = 'MIT')
+    {return 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt';};
+  if (license = 'Apache 2.0')
+    {return 'https://choosealicense.com/licenses/apache-2.0/';};
+  if (license = 'GPL 3.0')
+    {return 'https://github.com/Illumina/licenses/blob/master/gpl-3.0.txt';};
+  if (license = 'BSD 3')
+    {return 'https://github.com/thomasjbradley/micromodel/blob/master/BSD-3-CLAUSE-LICENSE.txt';}
+    else{
+      return 'No license is affiliated with this repository';
+    };
+}
+
+  
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
